8bdf71f83f5bdb29feb5497090ba83cd79e4f4fb
Naveed Choudhury <naveedchoudhury24@gmail.com>
2021-03-07 16:08:50 -0500

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,17 @@
 
+function shout(string) {
+  return string.toUpperCase();
+} 
+
 describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO');
   })
 })
+
+function whisper(string) {
+  return string.toLowerCase();
+} 
 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
